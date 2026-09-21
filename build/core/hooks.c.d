da34c21b03aa23de8fa2d07353cd65c0d19621b5
Dendrobyte <markbacon78@gmail.com>
2026-09-20 21:42:07 -0700

    Remove folders

--- build/core/hooks.c.d
+++
@@ -1,2 +0,0 @@
-/Users/markbacon/Documents/arduboy-simon/build/core/hooks.c.o: \
- /Users/markbacon/Library/Arduino15/packages/arduboy-homemade/hardware/avr/1.4.1/cores/arduboy/hooks.c
--- build/core/wiring_digital.c.d
+++
@@ -1,7 +0,0 @@
-/Users/markbacon/Documents/arduboy-simon/build/core/wiring_digital.c.o: \
- /Users/markbacon/Library/Arduino15/packages/arduboy-homemade/hardware/avr/1.4.1/cores/arduboy/wiring_digital.c \
- /Users/markbacon/Library/Arduino15/packages/arduboy-homemade/hardware/avr/1.4.1/cores/arduboy/wiring_private.h \
- /Users/markbacon/Library/Arduino15/packages/arduboy-homemade/hardware/avr/1.4.1/cores/arduboy/Arduino.h \
- /Users/markbacon/Library/Arduino15/packages/arduboy-homemade/hardware/avr/1.4.1/cores/arduboy/binary.h \
- /Users/markbacon/Library/Arduino15/packages/arduboy-homemade/hardware/avr/1.4.1/variants/arduboy-fx/pins_arduino.h \
- /Users/markbacon/Library/Arduino15/packages/arduboy-homemade/hardware/avr/1.4.1/variants/arduboy-fx/../arduboy-leonardo/pins_arduino.h
